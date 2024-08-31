1d08b2e152b512067889af1046d8de9134f761b7
Bay <bay@explorerhop.com>
2024-08-31 11:56:26 -0400

    8/31/2024

--- generator.py
+++ generator.py
@@ -7,19 +7,29 @@ document = Document()
 # Load a Word docx or doc document
 document.LoadFromFile("CoverLetter.docx")
 
-exactCompanyName = input("Exact Company Name: ")
-simpleCompanyName = input ("Simple/Shortened Company Name")
-companyAddress = input("Company Location/Address")
+exactCompanyName = input("\nExact Company Name: ")
+simpleCompanyName = input ("Simple/Shortened Company Name: ")
+if simpleCompanyName == "":
+    simpleCompanyName = exactCompanyName
+companyLocation = input("Company Location/Address: ")
 exactJobTitle = input("Exact Job Title: ")
 simpleTitle = input("Simple Job Title: ")
 foundThrough = input("How did you find this job?: ")
-
+impressedWith = input("I was impressed with... ")
 
 
 todayStr = str(date.today())
 
 document.Replace("[Date]", todayStr, False, False)
+document.Replace("[Exact Company Name]", exactCompanyName, False, False)
+document.Replace("[Company Location]", companyLocation, False, False)
+document.Replace("[Simple Company Name]", simpleCompanyName, False, False)
+document.Replace("[Exact Job Title]", exactCompanyName, False, False)
+document.Replace("[Simple Title]", simpleTitle, False, False)
+document.Replace("[Found Through]", foundThrough, False, False)
+document.Replace("[Impressed With]", impressedWith, False, False)
+
 
 # Save the resulting document
-document.SaveToFile("GENERATED.docx", FileFormat.Docx2016)
+document.SaveToFile("GENERATED - Delete Warning.docx", FileFormat.Docx2016)
 document.Close()
